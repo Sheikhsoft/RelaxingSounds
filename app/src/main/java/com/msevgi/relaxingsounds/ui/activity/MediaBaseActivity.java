e53251638877f24cd26e199cf8c1b45c86693cd4
Mustafa <mustafaasevgi@gmail.com>
2018-01-06 19:29:03 +0300

    added player and foreground service mechanizm

---
+++ app/src/main/java/com/msevgi/relaxingsounds/ui/activity/MediaBaseActivity.java
@@ -0,0 +1,8 @@
+package com.msevgi.relaxingsounds.ui.activity;
+
+/**
+ * Created by mustafasevgi on 6.01.2018.
+ */
+
+public class MediaBaseActivity {
+}
---
+++ app/src/main/java/com/msevgi/relaxingsounds/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.msevgi.relaxingsounds.utils;
+
+/**
+ * Created by mustafasevgi on 6.01.2018.
+ */
+
+public class Utils {
+}
